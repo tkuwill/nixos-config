3b28c131c5aca0dbf92701774f179b10247a075f
tkuwill <billwang1023@gmail.com>
2023-02-28 03:52:34 +0800

    Add the config of dunst.

---
+++ .config/.dwm/autostart.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+# General stuff
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+feh --bg-fill /home/will/Pictures/sysicon/wallpaper.png &
+fcitx5 &
+copyq &
+dunst &
+/home/will/.dwm/lowbatremind.sh &
+xrdb -merge ~/.Xresources &
+# xsetroot for dwm
+
+# caffeine
+print_caffeine(){
+    MODE=$(xset -q | grep 'DPMS is' | awk '{print $3}')
+    if [ "$MODE" = "Disabled" ]; then
+        printf ":零"
+    elif [ "$MODE" = "Enabled" ]; then
+        printf ":鈴"
+    fi
+}
+
+# dwm_date
+
+print_date(){
+	echo $(date "+%Y-%m-%d（%a）%T")
+}
+
+# by joestandring/dwm-bar
+
+dwm_battery () {
+    # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
+    CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
+    STATUS=$(cat /sys/class/power_supply/BAT0/status)
+
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        if [ "$STATUS" = "Charging" ]; then
+            printf "🔌 %s%% %s" "$CHARGE" "$STATUS"
+        else
+            printf "🔋 %s%% %s" "$CHARGE" "$STATUS"
+        fi
+    else
+        if [ "$STATUS" = "Charging" ]; then
+            printf "ﴞ %s%% %s" "$CHARGE" "$STATUS"
+        else
+            printf " %s%% %s" "$CHARGE" "$STATUS"
+        fi
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+# bat_time () {
+  # acpi | grep 'Battery 0' | grep  -Eo '[0-9][0-9]:[0-9][0-9]'
+# }
+
+dwm_alsa () {
+	STATUS=$(amixer sget Master | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")
+    VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+    	if [ "$STATUS" = "off" ]; then
+	            printf "🔇"
+    	else
+    		#removed this line becuase it may get confusing
+	        if [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+	            printf "🔈 %s%%" "$VOL"
+	        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+	            printf "🔉 %s%%" "$VOL"
+	        else
+	            printf "🔊 %s%%" "$VOL"
+	        fi
+		fi
+    else
+    	if [ "$STATUS" = "off" ]; then
+    		printf "MUTE"
+    	else
+	        # removed this line because it may get confusing
+	        if [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+	            printf "奔 %s%%" "$VOL"
+	        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+	            printf "墳 %s%%" "$VOL"
+	        elif [ "$VOL" = "0" ]; then
+	            printf "婢 %s%%" "$VOL"
+	        else
+	            printf " %s%%" "$VOL"
+        	fi
+        fi
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+while true
+do
+    xsetroot -name " $(print_caffeine)|$(dwm_alsa)|$(print_date)|$(dwm_battery)"
+    sleep 1 
+done
+
---
+++ .config/.dwm/screenlight.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# backlight up icon is from <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Ui icons created by Marz Gallery - Flaticon</a>.
+# backlight down icon is from <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Ui icons created by Marz Gallery - Flaticon</a>.
+# Credit from https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a & https://wiki.archlinux.org/title/Dunst  .
+
+# You can call this script like this:
+# $./screenlight.sh up
+# $./screenlight.sh down
+
+function get_brightness {
+    light -G
+}
+
+
+function send_notification {
+    brightness=`get_brightness`
+    # Send the notification
+     dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/brightness-up.png -t 8000 -r 2593 -u normal -h int:value:"$brightness" "Brightness: ${brightness}%"
+}
+
+function send_notification1 {
+    brightness=`get_brightness`
+    # Send the notification
+     dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/brightness-down.png -t 8000 -r 2593 -u normal -h int:value:"$brightness" "Brightness: ${brightness}%"
+}
+case $1 in
+    up)
+	# Set the brightness on 
+	
+	# Up the brightness (+ 2%)
+	light -A 2
+	send_notification
+	;;
+    down)
+	light -U 2
+	send_notification1
+	;;
+esac
