57f361644a255a35cb753b520d96e966b94c53d1
tkuwill <billwang1023@gmail.com>
2023-03-10 22:41:47 +0800

    Change the mouse shape from XC_xterm to XC_gumby .

--- st/config.def.h
+++ st/config.def.h
@@ -145,9 +145,9 @@ static unsigned int rows = 24;
 /*
  * Default colour and shape of the mouse cursor
  */
-static unsigned int mouseshape = XC_xterm;
-static unsigned int mousefg = 7;
-static unsigned int mousebg = 0;
+static unsigned int mouseshape = XC_gumby;
+static unsigned int mousefg = 9;
+static unsigned int mousebg = 9;
 
 /*
  * Color used to display font attributes when fontconfig selected a font which
