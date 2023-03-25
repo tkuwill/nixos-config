e9ddedacc8bc5e66ec348ece1e7938376a65ce54
tkuwill <billwang1023@gmail.com>
2023-03-25 20:24:00 +0800

    Add two scripts for reminder and battery record.

---
+++ shellscripts/reminder.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+echo "Input mins:"
+read mins
+echo "Input reminder messages:"
+read msg
+echo "Now time is "
+date "+%T"
+echo "Set a timer for ${mins} minute(s)."
+sleep ${mins}m && notify-send -u critical -t 10000 ${msg}
+mpv ~/Music/lovesong/大好きだよ｡.mp3 --start=100 --end=120 --keep-open=no --no-resume-playback --no-terminal --no-video
