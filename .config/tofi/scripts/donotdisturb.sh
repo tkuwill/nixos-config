0f5546791b3da29bd6bb9be7f5148182a9b2176b
tkuwill <billwang1023@gmail.com>
2023-06-02 22:03:42 +0800

    Fix the padding of tofi

--- .config/tofi/scripts/donotdisturb.sh
+++ .config/tofi/scripts/donotdisturb.sh
@@ -4,12 +4,15 @@
 if (ps -a | grep tofi >/dev/null); then
   killall -q tofi
 else
-  case $(printf "%s\n" "Cancel" "Notification center" "Do not disturb" "Normal" | tofi -c ~/.config/tofi/soy-milk) in
+  case $(printf "%s\n" "Cancel" "Notification center" "Clean all notifications" "Do not disturb" "Normal" | tofi -c ~/.config/tofi/soy-milk) in
   "Cancel")
     exit 0
     ;;
   "Notification center")
 	/home/will/shellscripts/notificationcenter.sh
+    ;;
+  "Clean all notifications")
+	dunstctl history-clear && dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/chat.png -t 8000 "Notifications history purged !"  
     ;;
   "Do not disturb")
         notify-send -i /home/will/Pictures/sysicon/bell.png -u critical -t 3000 "Do Not Disturb is now ON." && sleep 1s && dunstctl set-paused true
