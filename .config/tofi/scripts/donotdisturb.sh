6cd7a09a8d130035d6aa1e84479409b42f2bc16f
tkuwill <billwang1023@gmail.com>
2023-08-07 15:40:10 +0800

    Add new options in donotdisturn.sh and player.sh

--- .config/tofi/scripts/donotdisturb.sh
+++ .config/tofi/scripts/donotdisturb.sh
@@ -4,12 +4,15 @@
 if (ps -a | grep tofi >/dev/null); then
   killall -q tofi
 else
-  case $(printf "%s\n" "Cancel" "Notification center" "Clean all notifications" "Do not disturb" "Normal" | tofi -c ~/.config/tofi/soy-milk) in
+  case $(printf "%s\n" "Cancel" "Notification center" "Close all notifications" "Clean all notifications" "Do not disturb" "Normal" | tofi -c ~/.config/tofi/soy-milk) in
   "Cancel")
     exit 0
     ;;
   "Notification center")
 	/home/will/shellscripts/notificationcenter.sh
+    ;;
+  "Close all notifications")
+	dunstctl close-all
     ;;
   "Clean all notifications")
 	dunstctl history-clear && dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/chat.png -t 8000 "Notifications history purged !"  
--- .config/tofi/scripts/player.sh
+++ .config/tofi/scripts/player.sh
@@ -17,7 +17,7 @@ function burls {
 if (ps -a | grep tofi >/dev/null); then
   killall -q tofi
 else
-  case $(printf "%s\n" "Cancel" "Now playing" "Open with mpv Yt" "Open with mpv BiliBili" "Play-pause" "Next" "Prev" | tofi -c ~/.config/tofi/soy-milk) in
+  case $(printf "%s\n" "Cancel" "Now playing" "Open with mpv Yt" "Yt live" "Open with mpv BiliBili" "Play-pause" "Next" "Prev" | tofi -c ~/.config/tofi/soy-milk) in
   "Cancel")
     exit 0
     ;;
@@ -26,6 +26,9 @@ else
     ;;
   "Open with mpv Yt")
 	mpv $(urls)
+    ;;
+  "Yt live")
+	mpv --cache=no $(urls)
     ;;
   "Open with mpv BiliBili")
 	mpv --referrer="https://www.bilibili.com" $(burls)
